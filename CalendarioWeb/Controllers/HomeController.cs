777b67a28b7aef1b13ee39e57ee44a77cfb01648
josemeloph <contato.josemeloph@gmail.com>
2023-11-09 14:03:40 -0300

    Lógica para trocar o mês do calendário

--- CalendarioWeb/Controllers/HomeController.cs
+++ CalendarioWeb/Controllers/HomeController.cs
@@ -13,21 +13,51 @@ namespace CalendarioWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(Calendario cal)
         {
-            CultureInfo culture = CultureInfo.CurrentCulture;
-            Calendario calendario = new Calendario() { Dia = DateTime.Now.Day, Ano = DateTime.Now.Year, Mes = DateTime.Now.Month };
-            int[,] dias = calendario.GerarCalendario();
-            calendario.Dias = dias;
+            Calendario calendario = new Calendario() { Data = DateTime.Now };
+            calendario.GerarCalendario();
             return View(calendario);
         }
 
+        public IActionResult ProximoMes(DateTime data, int month)
+        {
+            int mes = data.Month;
+            int ano = data.Year;
+            if (mes == 12)
+            {
+                mes = 1;
+                ano ++;
+            }
+            else mes++;
+            data = new DateTime(ano, mes, data.Day);
+            Calendario cal = new Calendario { Data = data };
+            cal.GerarCalendario();
+            ViewData["semana"] = cal.Data.ToString("ddd");
+            ViewData["dia"] = cal.Data.Day;
+            return View("Index", cal);
+        }
+
+        public IActionResult MesAnterior(DateTime data, int month)
+        {
+            int mes = data.Month;
+            int ano = data.Year;
+            if (mes == 1)
+            {
+                mes = 12;
+                ano--;
+            }
+            else mes--;
+            data = new DateTime(ano, mes, data.Day);
+            Calendario cal = new Calendario { Data = data };
+            cal.GerarCalendario();
+            return View("Index", cal);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- CalendarioWeb/Models/Calendario.cs
+++ CalendarioWeb/Models/Calendario.cs
@@ -4,57 +4,42 @@ namespace CalendarioWeb.Models
 {
     public class Calendario
     {
-        public int Dia { get; set; }
-        public int Mes { get; set; }
-        public int Ano { get; set; }
-        public int[] Inicio_Fim {  get; set; }
-        public int[] QteDiasMes { get; set; } = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        public int[,] Dias {  get; set; }
+        public DateTime Data { get; set; }
+        public DateTime[,] Dias { get; set; } = new DateTime[6, 7];
 
-        public int PrimeiroDiaMes()
-        {
-            DateTime data = new DateTime(Ano, Mes, 1);
-            int diaSemana = (int)data.DayOfWeek;
-            return diaSemana;
-        }
 
-        public int[,] GerarCalendario()
+        public void GerarCalendario()
         {
-            if ((Ano % 4 == 0 && Ano % 100 != 0) || Ano % 400 == 0) QteDiasMes[1] = 29;
-            int[,] Dias = new int[6, 7];
-            int mesAtual = Mes - 1;
-            int mesAnterior = mesAtual == 0 ? 11 : mesAtual - 1;
+            DateTime data = Data;
+            int ano = data.Year;
+            int mes = data.Month;
+            int qteDias = DateTime.DaysInMonth(ano, mes);
+            int qteDiasMesAnterior = DateTime.DaysInMonth(ano, mes == 1 ? 12 : mes - 1);
+            int primeiroDiaMes = (int)new DateTime(ano, mes, 1).DayOfWeek;
             for (int d = 0; d < 7; d++)
             {
-                if (d < PrimeiroDiaMes()) Dias[0, d] = QteDiasMes[mesAnterior] - PrimeiroDiaMes() + d + 1;
-                else Dias[0, d] = d - PrimeiroDiaMes() + 1;
+                if (d < primeiroDiaMes) Dias[0, d] = new DateTime(data.Year, data.Month == 1 ? 12 : data.Month - 1, qteDiasMesAnterior - primeiroDiaMes + d + 1);
+                else Dias[0, d] = new DateTime(data.Year, data.Month, d - primeiroDiaMes + 1);
             }
-            Inicio_Fim = new int[3];
-            Inicio_Fim[0] = PrimeiroDiaMes();
-            int dia = Dias[0, 6] + 1;
+            int dia = Dias[0, 6].Day + 1;
             int diaMesSeguinte = 0;
             for (int i = 1; i < 6; i++)
             {
                 for (int j = 0; j < 7; j++)
                 {
-                    if (dia <= QteDiasMes[mesAtual])
+                    if (dia <= qteDias)
                     {
-                        Dias[i, j] = dia;
+                        Dias[i, j] = new DateTime(ano, mes, dia);
                         dia++;
                     }
                     else
                     {
-                        if(diaMesSeguinte == 0)
-                        {
-                            Inicio_Fim[1] = i;
-                            Inicio_Fim[2] = j - 1;
-                        }
                         diaMesSeguinte++;
-                        Dias[i, j] = diaMesSeguinte;
+                        Dias[i, j] = new DateTime(ano, mes == 12 ? 1 : mes + 1, diaMesSeguinte);
                     }
                 }
             }
-            return Dias;
         }
+
     }
 }
